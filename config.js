676cd6f6917838caf47395bcad4945aa56815236
kubik369 <kubik0369@gmail.com>
2017-03-22 21:19:02 +0100

    Extend Presto regex for 7th food, make its soup optional

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ var config = {
     veglife: process.env.OBEDBOT_VEGLIFE || '',
   },
   orderRegex: {
-    presto: /presto[1-6]p[1-2]/,
+    presto: /presto[1-7](p[1-2])?/,
     pizza: /pizza[0-9]{1,2}(v((33)|(40)|(50)))?/,
     veglife: /veg[1-4]\+?[ps]?/,
     spaghetti: /[a-z]{1,2}((300)|(400)|(450)|(600)|(800))((sc)|(cs)|(pc)|[psc])?\+?[pt]?/,
