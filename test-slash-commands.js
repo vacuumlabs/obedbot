770d84577da6aded5681499c69b6aa639b0d7862
Matej Krajčovič <matej@krajcovic.org>
2019-02-11 14:42:41 +0100

    Fix and refactor click menu parsing

--- test-slash-commands.js
+++ test-slash-commands.js
@@ -5,7 +5,7 @@ const utils = require('./build/utils');
   const presto = await utils.getMenu(config.menuLinks.presto, utils.parseTodaysPrestoMenu);
   const veglife = await utils.getMenu(config.menuLinks.veglife, utils.parseTodaysVeglifeMenu);
   const hamka = await utils.getMenu(config.menuLinks.hamka, utils.parseTodaysHamkaMenu);
-  const click = await utils.getMenu(config.menuLinks.click, utils.parseTodayClickMenu);
+  const click = await utils.getMenu(config.menuLinks.click, utils.parseTodaysClickMenu);
 
   console.log(veglife);
   console.log(presto);
