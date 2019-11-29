d2404b527d1ab07b110c2bde604a8f00c4cb819d
milan.kraus@vacuumlabs.com <Kraus Milan>
2019-11-29 12:35:20 +0100

    eslint

--- src/restaurants/utils.js
+++ src/restaurants/utils.js
@@ -6,8 +6,8 @@ export async function loadHtml(link) {
   const raw = await request({
     uri: link,
     headers: {
-      // eslint-disable-next-line max-len
       'User-Agent':
+        // eslint-disable-next-line max-len
         'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
     },
   })
